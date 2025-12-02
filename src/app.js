2058ea6a23f4e18b192ea1993da5566bbc51382c
Aravind Kumar P <aravind.panneer@gmail.com>
2025-12-02 20:33:58 +0530

    login and password encrypt api

--- src/app.js
+++ src/app.js
@@ -2,19 +2,55 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+const { validateSignupData } = require("./utils/validation");
 app.use(express.json());// convert json object to js object
+const bcrypt = require("bcrypt");
 
 
 app.post("/signup", async (req, res) => {
-
-    const user = new User(req.body);
-
     try{
+        validateSignupData(req);
+
+        const { firstName, lastName, emailId, password, age, gender, skills } = req.body;
+
+        const passwordHash = await bcrypt.hash(password, 10);
+        
+        const user = new User({
+            firstName,
+            lastName,
+            emailId,
+            password: passwordHash,
+            age,
+            gender,
+            skills
+        });
         await user.save();
-       // await user.save({ timestamps: { createdAt: true, updatedAt: false } });
         res.send("User Added Successfully!");
     }catch (err) {
-        res.status(400).send("Error saving the user:" + err.message);
+        res.status(400).send("Error :" + err.message);
+    }
+
+})
+
+app.post("/login", async (req, res) => {
+    try{
+
+       const { emailId, password } = req.body;
+       const user = await User.findOne({ emailId:emailId });
+       if(!user){
+
+           throw new Error("EmailId not found..!");
+       }
+       
+       const isPasswordValid = await bcrypt.compare(password, user.password);
+       if(isPasswordValid){
+            res.send("Login Successfull!");
+       } else {
+             throw new Error("Password is not correct..!");   
+       }
+
+    } catch (err) {
+        res.status(400).send("Error :" + err.message);
     }
 
 })

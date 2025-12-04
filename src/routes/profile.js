812fea21446ca906c5702cde5f36281d3fa12ada
Aravind Kumar P <aravind.panneer@gmail.com>
2025-12-04 13:37:41 +0530

    route file has been seperated

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -18,7 +18,7 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
     await loggedInUser.save();
     res.json({
-      message: ` ${loggedInUser.firstName}, Your profile updated successfully`,
+      message: ` ${loggedInUser.firstName}, Your profile updated successfully..!`,
       data: loggedInUser,
     });
   } catch (err) {

fa5fd6f74475e9faa3a16c5908bf9b57ad3aa057
Aravind Kumar P <aravind.panneer@gmail.com>
2025-12-10 17:07:04 +0530

    fixed changes

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () =>  {
-    await mongoose.connect(process.env.CONNECTION_DB_SECRET)
-    
-};
+const connectDB = async () =>  { await mongoose.connect(process.env.CONNECTION_DB_SECRET) };
 
 module.exports = connectDB;
